869e76139737981f252ced50a76395767d2060f4
Odin <odinuv@gmail.com>
2018-03-21 12:19:41 +0100

    refactor: change namespace

--- src/Component.php
+++ src/Component.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace MyComponent;
+namespace Keboola\ProcessorOrthogonal;
 
 use Keboola\Component\BaseComponent;
 use Keboola\Component\UserException;
--- src/Config.php
+++ src/Config.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace MyComponent;
+namespace Keboola\ProcessorOrthogonal;
 
 use Keboola\Component\Config\BaseConfig;
 
